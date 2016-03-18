b53589021e8bf3d0461de51a987b6337f6c9cacd
crystalxian <dzx07522@163.com>
2016-03-18 13:03:29 +0800

    Added files via upload
    The final vision

--- test.m
+++ test.m
@@ -3,20 +3,21 @@ figure(1);
 t=lineary_imagefilter(img);
 figure(2);
 y=lineary_imagefilter(t);
-figure(3);
 n=lineary_imagefilter(y);
-figure(4);
-m=lineary_imagefilter(n);
+figure(3);
+p=lineary_imagefilter(n);
+m=lineary_imagefilter(p);
 m1=m;
-figure(5);
-for i=1:10;
+figure(4);
+for i=1:15;
     m1=lineary_imagefilter(m1);
 end;
+
 imwrite(t,'Lena1.jpg','jpg');
-imwrite(y,'Lena2.jpg','jpg');
-imwrite(n,'Lena3.jpg','jpg');
-imwrite(m,'Lena4.jpg','jpg');
-imwrite(m1,'Lena5.jpg','jpg');
+imwrite(n,'Lena2.jpg','jpg');
+imwrite(m,'Lena3.jpg','jpg');
+imwrite(m1,'Lena4.jpg','jpg');
+
 
 
 
